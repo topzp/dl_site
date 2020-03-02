3b1b170f401a5397baefca4be1303031c18705dd
haomingdave <291053702@qq.com>
2020-03-02 13:28:42 +0800

    同上

--- app/Http/Controllers/PageViewController.php
+++ app/Http/Controllers/PageViewController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Service\WebService;
 use Illuminate\Http\Request;
 
 class PageViewController extends Controller
@@ -10,9 +11,9 @@ class PageViewController extends Controller
     public function index()
     {
         if (WebService::isMobile()) {
-            $returnView = "index.index_pc";
-        } else {
             $returnView = "index.index_mobile";
+        } else {
+            $returnView = "index.index_pc";
         }
         return view($returnView);
     }
